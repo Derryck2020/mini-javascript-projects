435384dbcfffc01ef3f65b58a12428d0595a9a3d
Derryck <oseiagyemanb@gmail.com>
2023-01-08 03:14:50 +0100

    update of files

--- counter/app.js
+++ counter/app.js
@@ -5,14 +5,14 @@ let count = 0;
 const value = document.querySelector('#value');
 const btns = document.querySelectorAll('.btn');
 
-//console.log(btns) //This will give a NodeList, which we can only perform only forEach array method on it
+//console.log(btns) //This will give a NodeList, which we can perform only forEach array method on it.
 
 //We selected all the buttons with forEach(), as we are looping through the buttons, we add an addEventListener for each click
 btns.forEach(el => { 
     el.addEventListener('click', function(e){
         //here we are using the event(e) object to get the currentTarget and classList property on each click
-        //console.log(e.currentTarget.classList); //we can console.log to check the classList available on the button
-        //let assign it to a variable styles which will save each time we click on our button
+        console.log(e.currentTarget.classList); //we can console.log to check the classList available on the button
+        //let assign it to a variable called styles which will save each time we click on our button
         const styles = e.currentTarget.classList;
         if(styles.contains('decrease')){
             count --;
@@ -20,11 +20,12 @@ btns.forEach(el => {
             count += 2;
         } else {
             count = 0;
+            //we set another if statments for colors
         } if (count > 0){
             value.style.color = 'green';
         } if (count < 0){
             value.style.color = 'red';
-        } if (value === 0) {
+        } if (count === 0) {
             value.style.color = 'black';
         };
         value.textContent = count;
